08621ae7aaf1b88292cfdb5bd6814104b663c84c
idrisyildiz <idrisyildiz7@gmail.com>
2024-06-06 12:00:06 +0300

    init

---
+++ croboxSDK.framework/Headers/croboxsdk.h
@@ -0,0 +1,18 @@
+//
+//  croboxsdk.h
+//  croboxsdk
+//
+//  Created by idris yıldız on 21.05.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for croboxsdk.
+FOUNDATION_EXPORT double croboxsdkVersionNumber;
+
+//! Project version string for croboxsdk.
+FOUNDATION_EXPORT const unsigned char croboxsdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <croboxsdk/PublicHeader.h>
+
+
